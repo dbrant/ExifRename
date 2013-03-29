f6fbbc6cc745741539f3a783aae4bfdc9a81aabc
Dmitry Brant <brant.dmitry@gmail.com>
2013-03-28 22:05:56 -0400

    Initial commit, migrated from svn.

---
+++ ExifRename/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace ExifRename
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnDo = new System.Windows.Forms.Button();
+            this.txtPhotoDir = new System.Windows.Forms.TextBox();
+            this.chkRecurse = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lnkAbout = new System.Windows.Forms.LinkLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPattern = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnDo
+            // 
+            this.btnDo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDo.Location = new System.Drawing.Point(243, 51);
+            this.btnDo.Name = "btnDo";
+            this.btnDo.Size = new System.Drawing.Size(149, 34);
+            this.btnDo.TabIndex = 0;
+            this.btnDo.Text = "Batch rename";
+            this.btnDo.UseVisualStyleBackColor = true;
+            this.btnDo.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtPhotoDir
+            // 
+            this.txtPhotoDir.AllowDrop = true;
+            this.txtPhotoDir.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPhotoDir.Location = new System.Drawing.Point(12, 25);
+            this.txtPhotoDir.Name = "txtPhotoDir";
+            this.txtPhotoDir.Size = new System.Drawing.Size(343, 20);
+            this.txtPhotoDir.TabIndex = 1;
+            this.txtPhotoDir.DragDrop += new System.Windows.Forms.DragEventHandler(this.txtPhotoDir_DragDrop);
+            this.txtPhotoDir.DragEnter += new System.Windows.Forms.DragEventHandler(this.txtPhotoDir_DragEnter);
+            // 
+            // chkRecurse
+            // 
+            this.chkRecurse.AutoSize = true;
+            this.chkRecurse.Checked = true;
+            this.chkRecurse.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkRecurse.Location = new System.Drawing.Point(15, 51);
+            this.chkRecurse.Name = "chkRecurse";
+            this.chkRecurse.Size = new System.Drawing.Size(154, 17);
+            this.chkRecurse.TabIndex = 3;
+            this.chkRecurse.Text = "Recurse into subdirectories";
+            this.chkRecurse.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Directory with photos (drag and drop):";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.lnkAbout);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtPattern);
+            this.groupBox1.Location = new System.Drawing.Point(12, 91);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(380, 102);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Rename pattern";
+            // 
+            // lnkAbout
+            // 
+            this.lnkAbout.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lnkAbout.AutoSize = true;
+            this.lnkAbout.BackColor = System.Drawing.Color.Transparent;
+            this.lnkAbout.Location = new System.Drawing.Point(329, 81);
+            this.lnkAbout.Name = "lnkAbout";
+            this.lnkAbout.Size = new System.Drawing.Size(44, 13);
+            this.lnkAbout.TabIndex = 4;
+            this.lnkAbout.TabStop = true;
+            this.lnkAbout.Text = "About...";
+            this.lnkAbout.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lnkAbout_LinkClicked);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.Location = new System.Drawing.Point(11, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(356, 50);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "[YYYY]: year    [MM]: month    [DD]: day\r\n[hh]: hour    [mm]: minute    [ss]: sec" +
+                "ond";
+            // 
+            // txtPattern
+            // 
+            this.txtPattern.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPattern.Location = new System.Drawing.Point(14, 19);
+            this.txtPattern.Name = "txtPattern";
+            this.txtPattern.Size = new System.Drawing.Size(353, 20);
+            this.txtPattern.TabIndex = 2;
+            this.txtPattern.Text = "[YYYY][MM][DD][hh][mm][ss]";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 200);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(404, 22);
+            this.statusStrip1.TabIndex = 6;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(48, 17);
+            this.lblStatus.Text = "Ready...";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBrowse.Location = new System.Drawing.Point(361, 23);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(31, 23);
+            this.btnBrowse.TabIndex = 7;
+            this.btnBrowse.Text = "...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 222);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.btnDo);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chkRecurse);
+            this.Controls.Add(this.txtPhotoDir);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDo;
+        private System.Windows.Forms.TextBox txtPhotoDir;
+        private System.Windows.Forms.CheckBox chkRecurse;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtPattern;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatus;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.LinkLabel lnkAbout;
+    }
+}
+
---
+++ ExifRename/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+/*
+    Copyright (C) 2011-2013 Dmitry Brant <me@dmitrybrant.com>
+  
+    This software is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+  
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+  
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write the Free Software Foundation, Inc., 51
+    Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ */
+namespace ExifRename
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = Application.ProductName;
+            FixDialogFont(this);
+        }
+
+        public static void FixDialogFont(Control c0)
+        {
+            Font old = c0.Font;
+            c0.Font = new Font(SystemFonts.MessageBoxFont.FontFamily.Name, old.Size, old.Style);
+            if (c0.Controls.Count > 0)
+                foreach (Control c in c0.Controls)
+                    FixDialogFont(c);
+        }
+
+
+
+        private void RenameDir(string directory, bool recurse)
+        {
+            try
+            {
+
+                var files = Directory.GetFiles(directory, "*.*");
+                string pattern = txtPattern.Text;
+                string tempStr;
+                char[] splitChars = { ':', ' ' };
+
+                for (int i = 0; i < files.Length; i++)
+                {
+                    try
+                    {
+                        lblStatus.Text = "Processing " + (i + 1).ToString() + " of " + files.Length.ToString();
+                        Application.DoEvents();
+
+                        if (!files[i].ToLower().Contains(".jp") && !files[i].ToLower().Contains(".mpo"))
+                            continue;
+
+                        string extension = Path.GetExtension(files[i]);
+                        string newName = "";
+                        {
+                            Image img = new Bitmap(files[i]);
+                            foreach (var p in img.PropertyItems)
+                            {
+                                if (p.Id == 0x9003)
+                                {
+                                    string[] poo = Encoding.ASCII.GetString(p.Value).Replace("\0", "").Split(splitChars);
+                                    if (poo.Length < 6) break;
+
+                                    tempStr = pattern;
+                                    tempStr = tempStr.Replace("[YYYY]", poo[0]);
+                                    tempStr = tempStr.Replace("[MM]", poo[1]);
+                                    tempStr = tempStr.Replace("[DD]", poo[2]);
+                                    tempStr = tempStr.Replace("[hh]", poo[3]);
+                                    tempStr = tempStr.Replace("[mm]", poo[4]);
+                                    tempStr = tempStr.Replace("[ss]", poo[5]);
+
+                                    newName = Path.GetDirectoryName(files[i]);
+                                    newName += "\\" + tempStr;
+                                    break;
+                                }
+                            }
+                            img.Dispose();
+                        }
+                        if (newName != "")
+                        {
+                            if (files[i] != newName)
+                            {
+                                for (int k = 1; k < 10; k++)
+                                {
+                                    if (File.Exists(newName + extension))
+                                        newName += "_";
+                                    else break;
+                                }
+                                newName += extension;
+                                File.Move(files[i], newName);
+                            }
+                        }
+
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(this, "Failed to process image " + files[i] + ":\n" + ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    }
+                }
+
+                if (recurse)
+                {
+                    var dirs = Directory.GetDirectories(directory);
+                    foreach (var dir in dirs)
+                    {
+                        RenameDir(dir, recurse);
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            btnDo.Enabled = false;
+            RenameDir(txtPhotoDir.Text, chkRecurse.Checked);
+            btnDo.Enabled = true;
+
+            lblStatus.Text = "Ready...";
+        }
+
+        private void txtPhotoDir_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, false) == true)
+                e.Effect = DragDropEffects.All;
+        }
+
+        private void txtPhotoDir_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (files.Length == 0) return;
+            txtPhotoDir.Text = files[0];
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            var dlg = new FolderBrowserDialog();
+            dlg.Description = "Select folder with photos to be renamed:";
+            if (dlg.ShowDialog(this) != DialogResult.OK) return;
+            txtPhotoDir.Text = dlg.SelectedPath;
+        }
+
+        private void lnkAbout_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            MessageBox.Show(this, "ExifRename - a simple photo renaming tool that uses EXIF timestamps.\n\nA Dmitry Brant joint.\nhttp://dmitrybrant.com", "About...", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+
+    }
+}
